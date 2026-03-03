fde52ae7b1110b6ae249caf58248af04d5a61edd
L-crown <bambooinside.pluto@outlook.jp>
2026-03-03 09:39:47 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,35 @@
+let gameData;
+let currentPAIndex = 0;
 
+const params = new URLSearchParams(location.search);
+const gameId = params.get("game");
+
+fetch(`live/${gameId}.json?${Date.now()}`)
+  .then(res => res.json())
+  .then(data => {
+    gameData = data;
+    init();
+  });
+
+function init(){
+  renderSummary();
+  renderScoreboard();
+  renderLineups();
+  renderZoneGrid();
+  renderCurrentPA();
+}
+
+function prevPA(){
+  if(currentPAIndex > 0){
+    currentPAIndex--;
+    renderCurrentPA();
+  }
+}
+
+function nextPA(){
+  const pas = [...new Set(gameData.pitches.map(p => p.pa_id))];
+  if(currentPAIndex < pas.length-1){
+    currentPAIndex++;
+    renderCurrentPA();
+  }
+}
