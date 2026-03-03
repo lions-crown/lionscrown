3c9957b6ee2ea5627890e38fd5b0331cb1993c9f
L-crown <bambooinside.pluto@outlook.jp>
2026-03-03 09:42:39 +0900

    Update stats.js

--- js/stats.js
+++ js/stats.js
@@ -1,1 +1,13 @@
-
+function calcPitchTypeRate(pitcherId){
+  const arr = gameData.pitches.filter(p=>p.pitcher_id===pitcherId);
+  const total = arr.length;
+  const map = {};
+  arr.forEach(p=>{
+    map[p.pitch_type] = (map[p.pitch_type]||0)+1;
+  });
+  let html="";
+  for(let k in map){
+    html += `${k}: ${(map[k]/total*100).toFixed(1)}%<br>`;
+  }
+  document.getElementById("pitcherStats").innerHTML = html;
+}
